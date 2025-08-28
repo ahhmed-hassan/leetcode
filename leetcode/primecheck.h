828ca953f2fd4b22be67d2ff562198206fc9abbd
Ahmed Hassan <ahmedhosam.1591@gmail.com>
2025-08-28 20:04:09 +0200

    Funcions_reanming

--- leetcode/primecheck.h
+++ leetcode/primecheck.h
@@ -8,7 +8,7 @@ namespace medianOfTwoSortedArrays
 {
 	using namespace std;
 
-	pair<size_t, size_t> GetMedianIndex(std::span<const int> range) {
+	pair<size_t, size_t> get_median_indeicies(std::span<const int> range) {
 		auto size = range.size();
 		if (size == 0) {
 			throw std::invalid_argument("NOOOO");
@@ -23,38 +23,38 @@ namespace medianOfTwoSortedArrays
 	}
 
 
-	double BothArraysLessThan2(span<const int> nums1, span<const int> nums2) {
+	double both_sizes_less_than_three(span<const int> nums1, span<const int> nums2) {
 		vector<int> result{};
 		result.resize(nums1.size() + nums2.size());
 		std::ranges::merge(nums1, nums2, result.begin());
-		auto [first, second] = GetMedianIndex(result);
+		auto [first, second] = get_median_indeicies(result);
 		//cout << result[first] << ' ' << result[second];
 		//cout << endl;
 		return (result[first] + result[second]) / 2.0;
 	}
 
-	double median_of_two_and_monster(span<const int> s1, span<const int> s2) {
+	double one_size_is_two_or_less(span<const int> s1, span<const int> s2) {
 		vector<int> medianContainer{};
 
 		if (s1.size() <= 2) {
 			swap(s1, s2);
 		};
-		auto [first, second] = GetMedianIndex(s1);
+		auto [first, second] = get_median_indeicies(s1);
 		medianContainer.insert(medianContainer.end(), s1.begin() + first - 1, s1.begin() + second + 1 + 1);
-		return BothArraysLessThan2(s2, medianContainer);
+		return both_sizes_less_than_three(s2, medianContainer);
 	}
 
 	
-	double median_of_two_big_sorted_spand(std::span<int const> spanWithSmallerMedian, std::span<int const> spanWithBigerMedian)
+	double median_of_two_big_sorted_spans(std::span<int const> spanWithSmallerMedian, std::span<int const> spanWithBigerMedian)
 	{
 		if (spanWithSmallerMedian.size() <= 2 && spanWithBigerMedian.size() <= 2) {
-			return BothArraysLessThan2(spanWithSmallerMedian, spanWithBigerMedian);
+			return both_sizes_less_than_three(spanWithSmallerMedian, spanWithBigerMedian);
 		}
 		else if (spanWithSmallerMedian.size() <= 2 || spanWithBigerMedian.size() <= 2) {
-			return median_of_two_and_monster(spanWithSmallerMedian, spanWithBigerMedian);
+			return one_size_is_two_or_less(spanWithSmallerMedian, spanWithBigerMedian);
 		}
-		auto smallerMedianIndicies = GetMedianIndex(spanWithSmallerMedian);
-		auto biggerMedianIndicies = GetMedianIndex(spanWithBigerMedian);
+		auto smallerMedianIndicies = get_median_indeicies(spanWithSmallerMedian);
+		auto biggerMedianIndicies = get_median_indeicies(spanWithBigerMedian);
 		if ((spanWithSmallerMedian[smallerMedianIndicies.first] + spanWithSmallerMedian[smallerMedianIndicies.second]) >
 			(spanWithBigerMedian[biggerMedianIndicies.first] + spanWithBigerMedian[biggerMedianIndicies.second]))
 		{
@@ -62,7 +62,7 @@ namespace medianOfTwoSortedArrays
 			swap(spanWithSmallerMedian, spanWithBigerMedian); 
 		}
 		auto minimalSharedCut = std::min(smallerMedianIndicies.first, biggerMedianIndicies.first); 
-		return median_of_two_big_sorted_spand(spanWithSmallerMedian.last(spanWithSmallerMedian.size() - minimalSharedCut),
+		return median_of_two_big_sorted_spans(spanWithSmallerMedian.last(spanWithSmallerMedian.size() - minimalSharedCut),
 			spanWithBigerMedian.first(spanWithBigerMedian.size() - minimalSharedCut));
 
 
@@ -71,10 +71,10 @@ namespace medianOfTwoSortedArrays
 
 	double MedianOfTwoSortedArrays(const vector<int>& nums1, const vector<int>& nums2) {
 		if (nums1.size() <= 2 && nums2.size() <= 2) {
-			return BothArraysLessThan2(nums1, nums2);
+			return both_sizes_less_than_three(nums1, nums2);
 		}
 		else if (nums1.size() <= 2 || nums2.size() <= 2) {
-			median_of_two_and_monster(nums1, nums2);
+			one_size_is_two_or_less(nums1, nums2);
 		}
 	}
 
